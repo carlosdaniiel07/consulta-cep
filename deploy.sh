2dadd6b718c7d0257272a5ca8491b785ec2f17a5
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2019-11-02 12:56:41 -0300

    Correção no arg --base-href

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e # termina o script com um código diferente de 0 se alguma coisa falhar
 
 # roda o script de build da nossa aplicação
-ng build --prod --base-href https://carlosdaniiel07.github.io/${GH_REF}/
+ng build --prod --base-href https://carlosdaniiel07.github.io/consulta-cep/
 
 # pull requests e commits para outras branches diferentes da master 
 # não devem fazer o deploy, isso é opcional caso queira deletar as próximas 6 linhas
